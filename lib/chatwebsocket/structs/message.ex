a04b6e80e9c98636fb88c91d07f3322717fd412d
Kalle Päplow <kalle.paeplow@gmail.com>
2021-03-23 14:21:44 +0100

    Fix: local datetime

--- lib/chatwebsocket/structs/message.ex
+++ lib/chatwebsocket/structs/message.ex
@@ -2,6 +2,18 @@ defmodule Chatwebsocket.Structs.Message do
   defstruct [:author, :content, :inserted]
 
   def create_message(%{"author" => author, "content" => content}) do
-    %Chatwebsocket.Structs.Message{author: author, content: content, inserted: DateTime.utc_now()}
+    %Chatwebsocket.Structs.Message{
+      author: author,
+      content: content,
+      inserted: get_local_datetime()
+    }
+  end
+
+  defp get_local_datetime() do
+    {:ok, datetime} =
+      NaiveDateTime.local_now()
+      |> DateTime.from_naive("Etc/UTC")
+
+    datetime
   end
 end
